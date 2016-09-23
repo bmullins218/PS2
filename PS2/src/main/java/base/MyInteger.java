bb9120f703340e0811dc3256c7641cb98bc76c27
billymullins <billymullins@billys-air.fios-router.home>
2016-09-22 20:55:14 -0400

    My PS2 files

---
+++ PS2/src/main/java/base/MyInteger.java
@@ -0,0 +1,138 @@
+package base;
+
+public class MyInteger {
+	package base;
+
+
+
+	public class MyInteger {
+
+	private int Value;
+
+
+	public MyInteger(int iValue){
+
+	Value = iValue;
+
+	}
+
+
+	public int GetNumber(){
+
+	return Value;
+
+	}
+
+
+	public boolean isEven(){
+
+	if(Value%2==0)
+
+	return true;
+
+	else
+
+	return false;
+
+	}
+
+	public boolean isOdd(){
+
+	if(Value%2!=0)
+
+	return true;
+
+	else
+
+	return false;
+
+	}
+
+	public boolean isPrime(){
+
+	int num = 0;
+
+	for(num=2;num<=Value/2;num++){
+
+	if(Value%num!= 0)
+
+	return true;
+
+	}
+
+	return false;
+
+	}
+
+	public static boolean isEven(int Value){
+
+	if(Value%2==0)
+
+	return true;
+
+	else
+
+	return false;
+
+	}
+
+	public static boolean isOdd(int Value){
+
+	if(Value%2!=0)
+
+	return true;
+
+	else
+
+	return false;
+
+	}
+
+	public static boolean isPrime(int Value){
+
+	int num =0;
+
+	for(num=2;num<=Value/2;num++){
+
+	if(Value%num!=0)
+
+	return true;
+
+	}
+
+	return false;
+
+	}
+
+	public static boolean isEven(MyInteger iValue){
+
+	return MyInteger.isEven(iValue.GetNumber());
+
+	}
+
+	public static boolean isOdd(MyInteger iValue){
+
+	return MyInteger.isOdd(iValue.GetNumber());
+
+	}
+
+	public static boolean isPrime(MyInteger iValue){
+
+	return MyInteger.isPrime(iValue.GetNumber());
+
+	}
+
+	public boolean equals(int iValue){
+
+	return GetNumber()==iValue;
+
+	}
+
+	public boolean equal(MyInteger iValue){
+
+	return equals(iValue.GetNumber());
+
+	}
+
+	}
+}
--- PS2/src/test/java/base/MyInteger_Test.java
+++ PS2/src/test/java/base/MyInteger_Test.java
@@ -9,11 +9,66 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MyInteger_Test {
-
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	public static void setUpBeforeClass() throws Exception{
 	}
+		static MyInteger var = new MyInteger(28);
+		static MyInteger var1 = new MyInteger(12);
+		static MyInteger var2 = new MyInteger(5);
+		static MyInteger var3 = new MyInteger(9);
+		
+		@Test
+		public void testIsEven() {
+			boolean expectedResult0 = true;
+			boolean actualResult0 = var.isEven();
+			assertEquals(expectedResult0,actualResult0);
+			boolean expectedResult1 = true;
+			boolean actualResult1 = var1.isEven();
+			assertEquals(expectedResult1,actualResult1);
+			boolean expectedResult2 = false;
+			boolean actualResult2 = var2.isEven();
+			assertEquals(expectedResult2,actualResult2);
+			boolean expectedResult3 = false;
+			boolean actualResult3 = var3.isEven();
+			assertEquals(expectedResult3,actualResult3);
+		}
+		
 
+	public static void setUpBeforeClass1() throws Exception{
+		}
+		static MyInteger num = new MyInteger(1);
+		static MyInteger num1 = new MyInteger(2);
+		static MyInteger num2 = new MyInteger(3);
+		static MyInteger num3 = new MyInteger(4);	
+	
+		@Test
+		public void testIsOdd() {
+			boolean expectedResult0 = true;
+			boolean actualResult0 = num.isOdd();
+			assertEquals(expectedResult0,actualResult0);
+			boolean expectedResult1 = false;
+			boolean actualResult1 = num1.isOdd();
+			assertEquals(expectedResult1,actualResult1);
+			boolean expectedResult2 = true;
+			boolean actualResult2 = num2.isOdd();
+			assertEquals(expectedResult2,actualResult2);
+			boolean expectedResult3 = false;
+			boolean actualResult3 = num3.isOdd();
+			assertEquals(expectedResult3,actualResult3);
+			
+		}
+		@Test
+	public void isPrime(){
+		MyInteger a = new MyInteger(2);
+		MyInteger b = new MyInteger(3);
+		MyInteger c = new MyInteger(4);
+		MyInteger d = new MyInteger(5);
+		assertTrue(MyInteger.isPrime(a));
+		assertTrue(MyInteger.isPrime(b));
+		assertFalse(MyInteger.isPrime(c));
+		assertTrue(MyInteger.isPrime(d));
+		
+		}
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 	}
@@ -25,10 +80,8 @@ public class MyInteger_Test {
 	@After
 	public void tearDown() throws Exception {
 	}
-
-	@Test
-	public void test() {
-		assertTrue(1==1);
-	}
-
 }
+
+	
+
+
